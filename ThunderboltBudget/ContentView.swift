4f5add7f0bee7d8c1872bfb49d8404bd9e5b8e3d
ekwipt <ekwipt@gmail.com>
2026-03-18 20:35:35 +1100

    Initial Commit

---
+++ ThunderboltBudget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ThunderboltBudget
+//
+//  Created by Ben McCarthy on 18/3/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ThunderboltBudget/ThunderboltBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThunderboltBudgetApp.swift
+//  ThunderboltBudget
+//
+//  Created by Ben McCarthy on 18/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct ThunderboltBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
